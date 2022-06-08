e7fafc41894ea4b9994bfee5edaf0ddb7b3008fe
Ángel David <gfadavid240@gmail.com>
2022-06-08 16:57:51 -0400

    Encapsulación
    Siguiendo el ejemplo de acuerdo al concepto.

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Person.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Person.java
@@ -20,3 +20,32 @@ public class Person {
   }
   
 }
+
+/*
+Encapsulación
+
+    El significado de Encapsulación es asegurarse de que los datos "sensibles" estén ocultos para los usuarios. Para lograr esto, debes
+
+     declarar variables/atributos de clase como privados
+     proporcionar métodos públicos get y set para acceder y actualizar el valor de una variable privada
+
+Obtener y establecer
+
+En el capítulo anterior aprendió que solo se puede acceder a las variables privadas dentro de la misma clase (una clase 
+externa no tiene acceso a ellas). Sin embargo, es posible acceder a ellos si proporcionamos métodos públicos de obtención
+y configuración.
+El método get devuelve el valor de la variable y el método set establece el valor. 
+La sintaxis de ambos es que comienzan con get o set, seguido del nombre de la variable, con la primera letra en mayúscula:
+
+El método get devuelve el valor del nombre de la variable. 
+El método set toma un parámetro (nuevoNombre) y lo asigna a la variable de nombre. La palabra clave this se utiliza para
+referirse al objeto actual.
+Sin embargo, como la variable de nombre se declara privada, no podemos acceder a ella desde fuera de esta clase:
+
+¿Por qué encapsulación?
+
+    Mejor control de los atributos y métodos de clase.
+    Los atributos de clase pueden ser de solo lectura (si solo usa el método get) o de solo escritura (si solo usa el método set)
+    Flexible: el programador puede cambiar una parte del código sin afectar otras partes
+    Mayor seguridad de los datos
+*/
--- mavenproject1/src/main/java/com/mycompany/mavenproject1/main.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/main.java
@@ -6,12 +6,22 @@ public class main {
   
   public static void main(String[] args) {
       
-      Moto moto = new Moto();
-      moto.acelerar();
-      
-      
-      Carro carro = new Carro();
-      carro.acelerar();
+      // ABSTRACCIÓN
+//      Moto moto = new Moto();
+//      moto.acelerar();
+//      
+//      
+//      Carro carro = new Carro();
+//      carro.acelerar();
 
+      // ENCAPSULACIÓN
+//      Person myObj = new Person();
+//      myObj.name = "John";  // error (Porque no se puede acceder a una variable con el modificador "privatte")
+//      System.out.println(myObj.name); // error  (desde orta clase como es éste el caso)
+        
+//      En su lugar, usamos los métodos getName() y setName() para acceder y actualizar la variable:
+        Person myObj = new Person();
+        myObj.setName("Ángel"); // Set the value of the name variable to "John"
+        System.out.println(myObj.getName());
   }
 }
